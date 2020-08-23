bd51c5efddc73da51250c2abf7fce235a1d72a8e
leegaocheng <46027786@qq.com>
2020-08-23 09:39:50 +0800

    添加Main

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+/**
+ * @author 李高丞
+ * @version 1.0
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.close();
+    }
+}
